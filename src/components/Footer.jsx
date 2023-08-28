d79a7ef2688a308c14a1d5e7ee07362632adf1de
KanekiKira <Mashrap07732262321@gmail.com>
2023-08-28 16:36:15 +0600

    add utility__footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import style from '../styles/footer.module.css'
 
 const Footer = () => {
     return (
         <footer>
             <div className="contactUs__footer"></div>
             <div className="middle__footer"></div>
-            <div className="utility__footer"></div>
+            <div className={style.container}>
+            <div className={style.utility__footer}>
+                <h2 className={style.utility__h2}>Utility Pages</h2>
+                <ul className={style.utility__list}>
+                    <li>Style Guide</li>
+                    <li>Password Protected</li>
+                    <li>Licences</li>
+                    <li>Changelog</li>
+                </ul>
+            </div>
+            </div>
         </footer>
     );
 };
