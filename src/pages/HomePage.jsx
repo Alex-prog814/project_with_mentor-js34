5a1dad59d6ef5230d02f410b3f3602e9576e5044
qryow <elmar20061602@gmail.com>
2023-08-29 17:38:45 +0600

    add components to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Firstdiv from "./homePage/Firstdiv";
 import BannerComp from "./homePage/Banner";
 import HomePageAboutUsBlock from "./homePage/HomePageAboutUsBlock";
+import WhyChooseUs from "./homePage/WhyChooseUs";
 
 const HomePage = () => {
     return (
-        <div style={{ width: "85%", margin: "0 auto" }}>
-            <Firstdiv />
-            <BannerComp />
-            <HomePageAboutUsBlock />
-        </div>
+      <>
+        {/*<div style={{ width: "85%", margin: "0 auto" }}>*/}
+          <Firstdiv />
+          <BannerComp />
+          <HomePageAboutUsBlock />
+          <WhyChooseUs />
+        {/*</div>*/}
+      </>
     );
 };
 
