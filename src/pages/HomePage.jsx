e1a5cd5a7b7f5a619a51c349d2f9130c542bbd5e
Alex-prog814 <aleks.98.x.28@gmail.com>
2023-08-30 10:03:17 +0600

    Change products styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Firstdiv from "./homePage/Firstdiv";
-import BannerComp from "./homePage/Banner";
-import HomePageAboutUsBlock from "./homePage/HomePageAboutUsBlock";
-import WhyChooseUs from "./homePage/WhyChooseUs";
-import Slider from "./homePage/Slider"
-import EcoFriendly from "./homePage/EcoFriendly";
+import Firstdiv from "./HomePage/Firstdiv";
+import BannerComp from "./HomePage/Banner";
+import HomePageAboutUsBlock from "./HomePage/HomePageAboutUsBlock";
+import WhyChooseUs from "./HomePage/WhyChooseUs";
+import Slider from "./HomePage/Slider"
+import EcoFriendly from "./HomePage/EcoFriendly";
 
 const HomePage = () => {
     return (
