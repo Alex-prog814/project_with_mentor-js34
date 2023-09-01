928e31694bd506f00e9aaff4ad3541afd6f1a683
Muss999 <mussangle1@gmail.com>
2023-09-01 13:52:07 +0600

    kdj

--- src/components/products/ProductEdit.jsx
+++ src/components/products/ProductEdit.jsx
@@ -2,73 +2,81 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getOneProduct,
-  editProduct,
+    getOneProduct,
+    editProduct,
 } from "../../store/products/productsActions";
 import { clearOneProductState } from "../../store/products/productsSlice";
+import styles from "./styles/Edit.module.css";
 
 const ProductEdit = () => {
-  const { loading, oneProduct } = useSelector((state) => state.products);
-  const [product, setProduct] = useState(oneProduct);
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+    const { loading, oneProduct } = useSelector((state) => state.products);
+    const [product, setProduct] = useState(oneProduct);
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    dispatch(getOneProduct({ id }));
-    return () => dispatch(clearOneProductState());
-  }, []);
+    useEffect(() => {
+        dispatch(getOneProduct({ id }));
+        return () => dispatch(clearOneProductState());
+    }, []);
 
-  useEffect(() => {
-    if (oneProduct) {
-      setProduct(oneProduct);
-    }
-  }, [oneProduct]);
+    useEffect(() => {
+        if (oneProduct) {
+            setProduct(oneProduct);
+        }
+    }, [oneProduct]);
 
-  return (
-    <>
-      {loading ? (
-
-        <h3>Loading...</h3>
-      ) : (
+    return (
         <>
-          {product && (
-            <>
-
-              <h3>{product.name}</h3>
-              <input
-                type="text"
-                placeholder="Picture"
-                onChange={(e) =>
-                  setProduct({ ...product, picture: e.target.value })
-                }
-                value={product.picture}
-              />
-              <img src={product.picture} alt="" width="100" height="100" />
-              <input
-                type="number"
-                placeholder="Price"
-                onChange={(e) =>
-                  setProduct({ ...product, price: e.target.value })
-                }
-                value={product.price}
-              />
-              <button
-                onClick={() => {
-                  dispatch(editProduct(product));
-                  navigate("/products");
-                }}
-              >
-                Save Changes
-              </button>
-
-            </>
-          )}
+            {loading ? (
+                <h3>Loading...</h3>
+            ) : (
+                <>
+                    {product && (
+                        <div>
+                            <h3>{product.name}</h3>
+                            <input
+                                type="text"
+                                placeholder="Picture"
+                                onChange={(e) =>
+                                    setProduct({
+                                        ...product,
+                                        picture: e.target.value,
+                                    })
+                                }
+                                value={product.picture}
+                            />
+                            <img
+                                src={product.picture}
+                                alt=""
+                                width="100"
+                                height="100"
+                            />
+                            <input
+                                type="number"
+                                placeholder="Price"
+                                onChange={(e) =>
+                                    setProduct({
+                                        ...product,
+                                        price: e.target.value,
+                                    })
+                                }
+                                value={product.price}
+                            />
+                            <button
+                                onClick={() => {
+                                    dispatch(editProduct(product));
+                                    navigate("/products");
+                                }}
+                            >
+                                Save Changes
+                            </button>
+                        </div>
+                    )}
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
-
 
 export default ProductEdit;
