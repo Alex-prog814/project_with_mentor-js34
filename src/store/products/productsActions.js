7d37ae800465d6bbecbdf9e31b40a9c281865ab4
Arzymatiq <keepa3003gmail.com>
2024-05-27 22:52:21 +0600

    arzymat

--- src/store/products/productsActions.js
+++ src/store/products/productsActions.js
@@ -4,46 +4,57 @@ import { PRODUCTS_API } from "../../helpers/consts";
 import { getTotalPages } from "../../helpers/functions";
 
 export const getProducts = createAsyncThunk(
-  "products/getProducts",
-  async (_, { getState }) => {
-    const { currentPage, currentCategory, search } = getState().products;
-    const categoryAndSearchParams = `q=${search}${currentCategory && `&type=${currentCategory}`}`; 
-    const pagesLimitParams = `?_page=${currentPage}&_limit=12`;
-    const totalPages = await getTotalPages(`${PRODUCTS_API}?${categoryAndSearchParams}`);
-    const res = await axios.get(`${PRODUCTS_API}${pagesLimitParams}&q=${search}${currentCategory && `&type=${currentCategory}`}`);
-    return { res, totalPages };
-  }
+    "products/getProducts",
+    async (_, { getState }) => {
+        const { currentPage, currentCategory, search } = getState().products;
+        const categoryAndSearchParams = `q=${search}${
+            currentCategory && `&type=${currentCategory}`
+        }`;
+        const pagesLimitParams = `?_start=${currentPage}&_limit=12`;
+        const totalPages = await getTotalPages(
+            `${PRODUCTS_API}?${categoryAndSearchParams}`
+        );
+        const res = await axios.get(
+            `${PRODUCTS_API}${pagesLimitParams}&q=${search}${
+                currentCategory && `&type=${currentCategory}`
+            }`
+        );
+        return { res, totalPages };
+    }
 );
 
 export const getOneProduct = createAsyncThunk(
-  "products/getOneProduct",
-  async ({ id }) => {
-    let res = await axios.get(`${PRODUCTS_API}/${id}`);
-    return res;
-  }
+    "products/getOneProduct",
+    async ({ id }) => {
+        let res = await axios.get(`${PRODUCTS_API}/${id}`);
+        return res;
+    }
 );
 
 export const editProduct = createAsyncThunk(
-  "products/editProduct",
-  async (editedObj, { dispatch }) => {
-    let res = await axios.patch(`${PRODUCTS_API}/${editedObj.id}`, editedObj);
-    dispatch(getProducts());
-    return res;
-  }
+    "products/editProduct",
+    async (editedObj, { dispatch }) => {
+        let res = await axios.patch(
+            `${PRODUCTS_API}/${editedObj.id}`,
+            editedObj
+        );
+        dispatch(getProducts());
+        return res;
+    }
 );
 
 export const createProduct = createAsyncThunk(
-  "products/createProduct",
-  async (newProductObj, { dispatch }) => {
-    let res = await axios.post(PRODUCTS_API, newProductObj);
-    dispatch(getProducts);
-  }
+    "products/createProduct",
+    async (newProductObj, { dispatch }) => {
+        let res = await axios.post(PRODUCTS_API, newProductObj);
+        dispatch(getProducts);
+    }
 );
 
 export const deleteProduct = createAsyncThunk(
-  'products/deleteProduct',
-  async ({ id }, { dispatch }) => {
-    await axios.delete(`${PRODUCTS_API}/${id}`);
-    dispatch(getProducts());
-  }
+    "products/deleteProduct",
+    async ({ id }, { dispatch }) => {
+        await axios.delete(`${PRODUCTS_API}/${id}`);
+        dispatch(getProducts());
+    }
 );
